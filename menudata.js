4a3a2b060dd90ea0b7b008506ec51679c6ae91a6
samlawer <samlawer2016@gmail.com>
2025-05-31 03:39:57 +0800

    asd

---
+++ menudata.js
@@ -0,0 +1,60 @@
+// JavaScript Document
+// menudata.js
+const menuData = {
+  logo: "📙 小本本备忘录",
+  menuItems: [
+    {
+      title: "语言表达",
+      items: [
+        { text: "👍夸娃话术", href: "/edu/biaoyang.html", icon: "icon01" },
+        { text: "😜土味情话", href: "/edu/tuweiqinghua.html", icon: "icon02" },
+        { text: "📖情感诗词表达", href: "/edu/shici.html", icon: "icon03" },
+        { text: "👱‍♀️英语口语", href: "/edu/yingyukouyu.html", icon: "icon04" }
+      ]
+    },
+    {
+      title: "处事智慧",
+      items: [
+        { text: "🌟鼓励自己", href: "/edu/guliziji.html", icon: "icon05" },
+        { text: "🧘🏻‍♂️人性箴言", href: "/edu/zhenyan.html", icon: "icon06" },
+        { text: "💖心理效应汇总", href: "/edu/xinlixiaoying.html", icon: "icon01" },
+        { text: "👀人性阴暗行为洞察", href: "/edu/renxingyinan.html", icon: "icon01" },
+        { text: "🤬骂醒自己", href: "/edu/maxingziji.html", icon: "icon01" },
+        { text: "🌷健康养生", href: "/edu/yangsheng.html", icon: "icon01" }
+      ]
+    },
+    {
+      title: "基础常识",
+      items: [
+        { text: "🌡儿童安全教育", href: "/edu/ertonganquan.html", icon: "icon01" }
+      ]
+    },
+    {
+      title: "知识",
+      items: [
+        { text: "家居风水备忘录", href: "/edu/jiajufengshui.html", icon: "icon01" },
+        { text: "店铺风水备忘录", href: "/edu/dianpufengshui.html", icon: "icon01" },
+        { text: "中国美术史", href: "/edu/cnart.html", icon: "icon01" },
+        { text: "国画口诀汇总", href: "/edu/guohuakoujue.html", icon: "icon01" }
+      ]
+    },
+    {
+      singleItem: true,
+      text: "电话求助热线汇总",
+      href: "/tools/gonggong-rexian.html",
+      icon: "icon01"
+    },
+    {
+      singleItem: true,
+      text: "胎教音乐合集",
+      href: "/tools/taijiao-music/index.html",
+      icon: "icon01"
+    },
+    {
+      singleItem: true,
+      text: "心灵加油站",
+      href: "/quotes/index.html",
+      icon: "icon01"
+    }
+  ]
+};
