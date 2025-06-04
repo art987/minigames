fd7057804bc3827dfe6065e79cb8a58aa488d77b
samlawer <samlawer2016@gmail.com>
2025-06-04 12:44:42 +0800

    Update data-b2b.js

--- edu/data-b2b.js
+++ edu/data-b2b.js
@@ -1,115 +1,105 @@
 const data = {
     title: {
-        h1: "120个常用货源网站备忘",
-        p: "汇聚多元批发网站，满足多样采购需求"
+        h1: "优质货源渠道备忘录",
+        p: "整合多元货源网站，满足各类采购需求"
     },
     head: {
-        title: "批发网站备忘录",
-        keywords: "批发网站, 综合批发, 五金批发, 家纺批发, 美妆批发",
-        description: "整合各类批发网站，为您提供一站式采购平台，涵盖综合、五金、家纺、美妆等多领域。"
+        title: "优质货源渠道备忘录",
+        keywords: "货源渠道, 批发网站, 跨境电商, 服装货源, 美妆货源",
+        description: "一份全面的货源渠道汇总，涵盖各类商品批发网站，为采购商提供丰富选择。"
     },
     keywords: [
-        "批发网站", "综合批发", "五金批发", "家纺批发", "美妆批发"
+        "货源网站", "批发平台", "跨境电商", "服装", "美妆", "母婴", "食品", "玩具", "五金", "电子"
     ],
     content: {
-        "综合批发|综合批发平台，满足多样采购需求": [
-            "<c><h4>阿里巴巴</h4><p>全球领先采购批发平台</p><nav>https://www.1688.com</nav></c>",
-            "<c><h4>环球资源</h4><p>整合现实与网络资源的服务商</p><nav>https://www.qqzyw.com</nav></c>",
-            "<c><h4>环球华品</h4><p>提供多元商品批发</p><nav>https://www.chinabrands.cn</nav></c>",
-            "<c><h4>义乌购</h4><p>全球小商品批发平台</p><nav>https://www.yiwugo.com</nav></c>",
-            "<c><h4>53货源</h4><p>提供网店代理代销及一件代发</p><nav>https://www.53shop.com</nav></c>",
-            "<c><h4>买购网</h4><p>品牌与知识排行榜门户</p><nav>https://www.maigoo.com</nav></c>",
-            "<c><h4>乐万汇批发城</h4><p>小商品混批及配送平台</p><nav>https://www.sphunpi.com</nav></c>",
-            "<c><h4>北极光供应链</h4><p>专业供应链服务</p><nav>https://www.aurorascm.com</nav></c>",
-            "<c><h4>浙江名营企业网</h4><p>浙江中小企业网上贸易中心</p><nav>https://www.zj123.com</nav></c>"
+        "综合批发|全品类综合批发平台": [
+            "<c><h4>1688</h4><p>各类目货源，支持一键铺货</p><nav>https://www.1688.com</nav></c>",
+            "<c><h4>53货源网</h4><p>专业的微商、淘宝网店代理货源网，综合型批发网站</p><nav>https://www.53shop.com</nav></c>",
+            "<c><h4>义乌购</h4><p>覆盖全国小商品产业，可360°全景看店铺</p><nav>https://www.yiwugo.com</nav></c>",
+            "<c><h4>八方资源网</h4><p>电子商务门户网站，自由网上贸易</p><nav>https://www.b2b168.com</nav></c>",
+            "<c><h4>世界工厂网</h4><p>全球电子商务互动平台</p><nav>https://www.gongchang.com</nav></c>",
+            "<c><h4>环球贸易网</h4><p>涵盖五金、建材等多行业</p><nav>https://www.globalimporter.net</nav></c>",
+            "<c><h4>马可波罗网</h4><p>全球精准采购搜索引擎</p><nav>https://www.makepolo.com</nav></c>",
+            "<c><h4>买购网</h4><p>名牌导购、精华推荐平台</p><nav>https://www.maigoo.com</nav></c>"
         ],
-        "五金批发|专业五金工业批发平台": [
-            "<c><h4>中国制造网</h4><p>专注中国制造的B2B平台</p><nav>https://www.cn.made-in-china.com</nav></c>",
-            "<c><h4>中国供应商网</h4><p>免费B2B信息发布及采购平台</p><nav>https://www.cn.china.cn</nav></c>",
-            "<c><h4>慧聪网</h4><p>中小企业经营服务平台</p><nav>https://www.hc360.com</nav></c>",
-            "<c><h4>工品汇</h4><p>一站式工业用品采购平台</p><nav>https://www.vipmro.com</nav></c>",
-            "<c><h4>工邦邦</h4><p>服务工业用品经销商</p><nav>https://www.gongbangbang.com</nav></c>",
-            "<c><h4>西域</h4><p>MRO数字供应链专家</p><nav>https://www.ehsy.com</nav></c>",
-            "<c><h4>网商网</h4><p>网站建设与推广平台</p><nav>https://www.onwsw.com</nav></c>",
-            "<c><h4>八方资源网</h4><p>营造自由网上贸易</p><nav>https://www.b2b168.com</nav></c>",
-            "<c><h4>世界工厂网</h4><p>企业线上生态建设服务先行者</p><nav>https://www.gongchang.com</nav></c>",
-            "<c><h4>环球贸易网</h4><p>连接商务资讯与网络贸易</p><nav>https://www.china.herostart.com</nav></c>",
-            "<c><h4>汽配人</h4><p>汽车配件批发网</p><nav>https://www.qipeiren.com/peijian</nav></c>",
-            "<c><h4>产品网</h4><p>工程机械相关产品平台</p><nav>https://www.21 - mars.com</nav></c>",
-            "<c><h4>一呼百应网</h4><p>工业品直卖网</p><nav>https://www.b2b.youboy.com</nav></c>",
-            "<c><h4>华强电子网</h4><p>IC/电子元器件采购交易平台</p><nav>https://www.hqew.com</nav></c>",
-            "<c><h4>猎芯网</h4><p>全球电子供应链智慧平台</p><nav>https://www.ichunt.com</nav></c>"
+        "外贸跨境|跨境电商及外贸货源平台": [
+            "<c><h4>环球资源</h4><p>外贸相关平台</p><nav>https://www.globalsources.com</nav></c>",
+            "<c><h4>环球华品</h4><p>全球一件代发平台</p><nav>https://www.chinabrands.com</nav></c>",
+            "<c><h4>北极光供应链</h4><p>进口货源，跨境供应链平台</p><nav>https://www.aurorascm.com</nav></c>",
+            "<c><h4>敦煌网</h4><p>跨境电商B2B平台</p><nav>https://www.dhgate.com</nav></c>",
+            "<c><h4>青创网</h4><p>中小电商卖家男装进货批发平台</p><nav>https://www.qcworld.cn</nav></c>"
         ],
-        "家纺批发|专业家纺家居批发渠道": [
-            "<c><h4>91家纺网</h4><p>叠石桥网销导航及供货平台</p><nav>https://www.91jf.com</nav></c>"
+        "五金工业|五金工业用品批发平台": [
+            "<c><h4>浙江名营企业网</h4><p>提供五金、工业、机械、电子等货源</p><nav>未找到确切网址</nav></c>",
+            "<c><h4>中国制造网</h4><p>B2B丰富产品资源，侧重工业、电子</p><nav>https://cn.made-in-china.com</nav></c>",
+            "<c><h4>中国供应商网</h4><p>提供工业机械、五金货源</p><nav>https://cn.china.cn</nav></c>",
+            "<c><h4>西域</h4><p>一站式mro工业品平台</p><nav>https://www.xiyuyun.com</nav></c>",
+            "<c><h4>工邦邦</h4><p>低价正品工业用品平台</p><nav>https://www.gongbangbang.com</nav></c>"
+        ],
+        "服装批发|丰富多样的服装批发渠道": [
+            "<c><h4>广州17网</h4><p>广州服装批发，主做女装一手货源</p><nav>https://gz.17zwd.com</nav></c>",
+            "<c><h4>四季星座</h4><p>以男装为主，有自己的分销APP</p><nav>https://www.571xz.com</nav></c>",
+            "<c><h4>衣联网</h4><p>服装批发市场</p><nav>https://www.eelly.com</nav></c>",
+            "<c><h4>搜款网</h4><p>广州最大的网批平台，提供女装一手货源</p><nav>https://www.vvic.com</nav></c>",
+            "<c><h4>网商园</h4><p>可提供男装等货源</p><nav>https://www.wsy.com</nav></c>",
+            "<c><h4>中国女装网</h4><p>专注女装的平台</p><nav>https://www.chinady.com</nav></c>",
+            "<c><h4>货捕头</h4><p>提供浙江服饰鞋包、童装货源</p><nav>https://www.hznzcn.com</nav></c>",
+            "<c><h4>一起做网店</h4><p>提供女装，可一件代发</p><nav>https://www.17zwd.com</nav></c>"
+        ],
+        "鞋类批发|专业鞋类批发平台": [
+            "<c><h4>环球鞋网</h4><p>鞋行业垂直电子商务平台</p><nav>https://www.shoes.net.cn</nav></c>",
+            "<c><h4>开山网</h4><p>专门做女鞋的货源网站</p><nav>https://k3.cn/web/welcome</nav></c>",
+            "<c><h4>中国鞋库网</h4><p>鞋子实体批发，提供一件代发</p><nav>https://www.cnxieku.com</nav></c>",
+            "<c><h4>爱买卖</h4><p>台州温岭女鞋批发货源平台</p><nav>https://www.2mm.cn</nav></c>",
+            "<c><h4>新款网</h4><p>专业男鞋货源平台</p><nav>https://www.xingfujie.cn</nav></c>",
+            "<c><h4>中国鞋网</h4><p>全球专业的中文鞋类平台</p><nav>https://www.chinashoesnet.com</nav></c>"
         ],
         "美妆批发|美妆产品批发优选": [
-            "<c><h4>NALA美妆网</h4><p>为全球美妆商家提供采销服务</p><nav>https://www.nala.com.cn</nav></c>",
-            "<c><h4>个秀名妆网</h4><p>韩国化妆品批发分销平台</p><nav>https://www.geshow.com</nav></c>",
-            "<c><h4>中妆网</h4><p>美妆相关产品批发</p><nav>https://www.zghzp.com</nav></c>",
-            "<c><h4>优分销</h4><p>提供美妆分销服务</p><nav>https://www.alifenxiao.com</nav></c>"
+            "<c><h4>个秀名妆</h4><p>主营韩国日本欧美的保养品和化妆品</p><nav>https://www.geshow.com</nav></c>",
+            "<c><h4>NALA娜拉美妆网</h4><p>国内知名的美妆采购批发网站</p><nav>https://www.nala.com.cn</nav></c>"
         ],
-        "珠宝首饰批发|珠宝首饰批发平台": [
-            "<c><h4>批发户</h4><p>珠宝首饰批发网站</p><nav>https://www.pfhoo.com</nav></c>"
+        "母婴批发|母婴用品专业批发": [
+            "<c><h4>挚爱母婴</h4><p>母婴产品批发平台，支持一件代发</p><nav>https://www.babyzhiai.net</nav></c>",
+            "<c><h4>织里童装网</h4><p>湖州织里本地的童装城行业网站</p><nav>https://www.zhilitongzhuang.com</nav></c>",
+            "<c><h4>生意网</h4><p>湖州织里童装批发网站，品类全</p><nav>https://www.3e3e.cn</nav></c>"
         ],
-        "食品批发|食品采购批发优选": [
-            "<c><h4>先冻网</h4><p>食品批发相关平台</p><nav>https://www.xiandongmall.com/xdmall</nav></c>",
-            "<c><h4>58食品网</h4><p>食品招商加盟代理平台</p><nav>https://www.58food.com</nav></c>",
-            "<c><h4>食品代理网</h4><p>专业食品代理招商平台</p><nav>https://www.spdl.com</nav></c>",
-            "<c><h4>众人店</h4><p>土特产网店代运营及货源推广</p><nav>https://www.zhongrendian.com</nav></c>"
+        "食品批发|食品酒水采购平台": [
+            "<c><h4>58食品网</h4><p>主要经营奶制品、饮品等批发</p><nav>https://www.58food.com</nav></c>",
+            "<c><h4>众人店</h4><p>特产批发平台</p><nav>https://www.zhongrendian.com</nav></c>",
         ],
-        "医药成人用品批发|医药成人用品批发渠道": [
-            "<c><h4>360好药网</h4><p>医药批发平台</p><nav>https://www.360haoyao.com</nav></c>",
-            "<c><h4>性商网</h4><p>情趣用品批发平台</p><nav>https://www.chinasexq.com</nav></c>"
+        "玩具批发|专业玩具批发网站": [
+            "<c><h4>玩具巴巴</h4><p>专业玩具B2B网站</p><nav>https://www.toybaba.com</nav></c>"
         ],
-        "母婴童装玩具批发|母婴童装玩具批发天地": [
-            "<c><h4>挚爱母婴网</h4><p>高端母婴用品B2B分销平台</p><nav>https://www.babyzhiai.net</nav></c>",
-            "<c><h4>玩具巴巴</h4><p>玩具批发专业网站</p><nav>https://www.toybaba.com</nav></c>",
-            "<c><h4>生意网</h4><p>童装童鞋批发厂家直销平台</p><nav>https://www.3e3e.cn</nav></c>",
-            "<c><h4>乐贝贝童装批发网</h4><p>外贸童装批发厂家直销</p><nav>https://www.lebeibei.com</nav></c>",
-            "<c><h4>织里童装网</h4><p>湖州织里本地童装批发网站</p><nav>https://www.zhilitongzhuang.com</nav></c>"
+        "珠宝饰品批发|珠宝首饰批发定制": [
+            "<c><h4>梅陇珠宝</h4><p>珠宝首饰批发/定制平台</p><nav>未找到确切网址</nav></c>",
+            "<c><h4>批发户</h4><p>提供时尚饰品、纯银首饰货源</p><nav>https://www.pfhoo.com</nav></c>"
         ],
-        "服装批发|丰富服装批发资源": [
-            "<c><h4>四季星座</h4><p>提供四季青服装货源及网店代理</p><nav>https://www.571xz.com</nav></c>",
-            "<c><h4>骏韵服饰批发网</h4><p>服饰批发相关平台</p><nav>https://www.shuiyipifa.cn</nav></c>",
-            "<c><h4>中国服装</h4><p>涵盖服装多领域资讯与服务</p><nav>https://www.efu.com.cn</nav></c>",
-            "<c><h4>中国女装网</h4><p>女装品牌信息与招商平台</p><nav>https://www.43er.com</nav></c>",
-            "<c><h4>档口网</h4><p>品质女装及羽绒服批发平台</p><nav>https://www.dangkou.net</nav></c>",
-            "<c><h4>聚衣网</h4><p>服装批发相关网站</p><nav>https://www.juyi5.cn</nav></c>",
-            "<c><h4>货捕头</h4><p>杭州女装批发代理及一件代发</p><nav>https://www.hznzcn.com</nav></c>",
-            "<c><h4>搜款网</h4><p>广州服装批发及一件代发平台</p><nav>https://www.vvic.com/gz</nav></c>",
-            "<c><h4>杭州的网商园</h4><p>专业网店货源批发分销平台</p><nav>https://www.wsy.com</nav></c>",
-            "<c><h4>广州17网</h4><p>广州服装批发及一手货源平台</p><nav>https://www.gz.17zwd.com</nav></c>",
-            "<c><h4>衣联网</h4><p>服装批发市场领航者</p><nav>https://www.eelly.com</nav></c>",
-            "<c><h4>一起做网店</h4><p>广州服装批发及一手货源平台</p><nav>https://www.gz.17zwd.com</nav></c>",
-            "<c><h4>我要做网店</h4><p>网络服装批发及一手货源平台</p><nav>https://www.51zwd.com</nav></c>",
-            "<c><h4>杭州女装网</h4><p>杭州女装批发代理及一件代发</p><nav>https://www.hznzcn.com</nav></c>",
-            "<c><h4>315货源网</h4><p>提供服装等货源批发</p><nav>https://www.315hyw.com</nav></c>",
-            "<c><h4>深圳地摊网</h4><p>专注地摊货源批发</p><nav>https://www.szbdt.com</nav></c>",
-            "<c><h4>聚衣网（中老年女装）</h4><p>中老年女装批发相关</p><nav>https://www.juyi5.cn</nav></c>",
-            "<c><h4>批批网</h4><p>服装等产品批发平台</p><nav>https://www.pipipifa.com</nav></c>",
-            "<c><h4>全球内衣网（内衣）</h4><p>内衣批发相关网站</p><nav>https://www.www.ny.cn</nav></c>"
+        "家纺批发|家纺产品批发资源": [
+            "<c><h4>91家纺网</h4><p>提供南通叠石桥家纺四件套等床品货源</p><nav>https://www.91jf.com</nav></c>"
         ],
-        "鞋包批发|鞋包采购批发平台": [
-            "<c><h4>台州的开山网</h4><p>女鞋货源批发代销平台</p><nav>https://www.k3.cn/web/welcome</nav></c>",
-            "<c><h4>中国鞋网</h4><p>全球专业鞋类加盟门户</p><nav>https://www.cnxz.cn</nav></c>",
-            "<c><h4>鞋都网</h4><p>鞋子批发网上商城</p><nav>https://www.800vi.com</nav></c>",
-            "<c><h4>爱搜鞋</h4><p>一手鞋类货源平台</p><nav>https://www.sooxie.com</nav></c>",
-            "<c><h4>新款网</h4><p>鞋类网销货源及一件代发</p><nav>https://www.xingfujie.cn/web</nav></c>",
-            "<c><h4>爱买卖</h4><p>温岭女鞋批发平台</p><nav>https://www.2mm.cn</nav></c>",
-            "<c><h4>美美淘</h4><p>女鞋厂家及分销平台</p><nav>https://www.mmgg.com</nav></c>", 
-            "<c><h4>中国鞋库网</h4><p>鞋类批发相关网站</p><nav>https://www.cnxieku.com/index</nav></c>",
-            "<c><h4>购途网</h4><p>女鞋批发及货源平台</p><nav>https://www.go2.cn</nav></c>",
-            "<c><h4>鞋裤网</h4><p>鞋类批发相关网站</p><nav>https://www.cnxieku.com/index</nav></c>",
-            "<c><h4>环球鞋网</h4><p>鞋类相关批发网站</p><nav>https://www.shoes.net.cn</nav></c>",
-            "<c><h4>货源之家</h4><p>可信货源及加盟平台</p><nav>https://www.huoyuanzhijia.com</nav></c>",
-            "<c><h4>箱包网</h4><p>箱包专业市场与人脉服务平台</p><nav>https://www.31bag.com</nav></c>",
-            "<c><h4>包牛牛</h4><p>白沟网包批发及一件代发</p><nav>https://www.bao66.cn/web</nav></c>",
-            "<c><h4>亚马逊</h4><p>一站放心购全球</p><nav>https://www.amazon.cn</nav></c>",
-            "<c><h4>全球速卖通</h4><p>在线购物平台</p><nav>https://www.aliexpress.com</nav></c>",
-            "<c><h4>贸管家</h4><p>相关贸易服务平台</p><nav>https://www.balalaba.com</nav></c>",
-            "<c><h4>兰亭集势</h4><p>跨境电商相关平台</p><nav>https://www.supplierportal.litb.cn/metis</nav></c>",
-            "<c><h4>郭煌网</h4><p>中小商家跨境电商外贸平台</p><nav>https://www.seller.dhgate.com</nav></c>"
+        "内衣睡衣批发|内衣睡衣专业批发": [
+            "<c><h4>全球内衣网</h4><p>中国专业的睡衣，内衣裤，家居服等供货批发平台</p><nav>https://www.ny.cn</nav></c>"
+        ],
+        "电子批发|电子元器件采购平台": [
+            "<c><h4>华强电子网</h4><p>电子元器件原材料采购垂直B2B网站</p><nav>https://www.hqew.com</nav></c>",
+            "<c><h4>猎芯网</h4><p>专业IC芯片、电子元器件采购商城平台</p><nav>https://www.ichunt.com</nav></c>",
+            "<c><h4>DX（Dealextreme）</h4><p>主要针对小工具和电子产品做一件代发和批发的平台</p><nav>https://www.dx.com</nav></c>"
+        ],
+        "医药批发|医药产品批发平台": [
+            "<c><h4>360好药网</h4><p>医药批发，品类齐全</p><nav>未找到确切网址</nav></c>"
+        ],
+        "DIY及定制批发|DIY产品及定制货源平台": [
+            "<c><h4>SDS定制</h4><p>专注DIY产品铺货平台</p><nav>未找到确切网址</nav></c>",
+            "<c><h4>指纹科技</h4><p>选品、设计、生产、物流一站式平台</p><nav>未找到确切网址</nav></c>"
+        ],
+        "花卉批发|鲜花批发平台": [
+            "<c><h4>花伍</h4><p>花田直发的云南鲜花批发APP</p><nav>https://www.hua5.com/</nav></c>"
+        ],
+        "农产品批发|农产品采购平台": [
+            "<c><h4>惠农网</h4><p>农产品批发平台，涵盖水果、蔬菜等</p><nav>https://www.cnhnb.com</nav></c>"
+        ],
+        "餐饮批发|餐饮行业批发平台": [
+            "<c><h4>快驴进货</h4><p>美团旗下餐饮行业批发平台</p><nav>https://kuailv.meituan.com</nav></c>"
         ]
     }
 };
