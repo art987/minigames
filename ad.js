8a868b0fcc7227512f2f0e1a5014ad396575b919
samlawer <114122987+art987@users.noreply.github.com>
2026-07-04 19:30:28 +0800

    8888

--- ad.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("ad1").innerHTML = '<p>广告位 1</p>';
-    document.getElementById("ad2").innerHTML = '<p>广告位 2</p>';
-});
